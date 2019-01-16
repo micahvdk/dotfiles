bf1bdacdd1cff05d3b76d51e5a0faf66cc722c27
Greg Cook <greg@gregcook.com>
2019-01-17 09:21:36 +1100

    refactor editor installer

--- editor.sh
+++ editor.sh
@@ -1,29 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 PS3='Please enter your editor of choice: '
-options=("Atom" "VS Code" "VIM" "Sublime" "Exit")
+QUIT="Quit Install"
+touch "$QUIT"
+
+options=("atom" "vscode" "vim" "sublime" "$QUIT")
+
 select opt in "${options[@]}"
 do
     case $opt in
-        "Atom")
-            echo "Installing and setting up Atom"
-            source editor/atom/install.sh
+        "$QUIT")
+            echo "Exiting..."
+	    break
             ;;
-        "VS Code")
-            echo "Installing and setting up VS Code"
-            source editor/vscode/install.sh
+        *)
+            echo "Installing and setting up $opt"
+            source editor/$opt/install.sh
             ;;
-        "VIM")
-            echo "Installing and setting up VIM"
-            source editor/vim/install.sh
-            ;;
-        "Sublime")
-            echo "Installing and setting up Sublime Text"
-            source editor/sublime/install.sh
-            ;;
-        "Exit")
-            echo "Goodbye"
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
     esac
 done
+rm "$QUIT"
