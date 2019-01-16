b64bfa9ca884d900772d78dfcbe156b98a378f16
Greg Cook <greg@gregcook.com>
2019-01-16 15:55:18 +1100

    bugfixes

--- pre-install.sh
+++ pre-install.sh
@@ -6,15 +6,13 @@ if [ ! -d "$OMZDIR" ]; then
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
   echo 'Updating oh-my-zsh'
-  # upgrade_oh_my_zsh
-  chmod +x $OMZDIR/tools/upgrade.sh
-  $OMZDIR/tools/upgrade.sh
+  upgrade_oh_my_zsh
 fi
 
 which -s brew
 if [[ $? != 0 ]] ; then
   echo "Homebrew not found. Installing..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   echo "Homebrew already installed! Updating..."
   brew update
