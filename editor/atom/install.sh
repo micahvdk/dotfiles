77aed0cbb699e5db0c72294aad90756de52e85b6
Greg Cook <greg@gregcook.com>
2019-01-21 12:24:04 +1100

    tweaks

--- editor/atom/install.sh
+++ editor/atom/install.sh
@@ -1,21 +1,18 @@
-brew cask install "atom"
-apm install Sublime-Style-Column-Selection
+brew cask install atom
 apm install atom-beautify
-apm install atom-formatter-jsbeautify
+apm install atom-yamljson
 apm install busy-signal
 apm install change-case
 apm install editorconfig
-apm install file-icons@
+apm install file-icons
+apm install file-types
 apm install go-debug
 apm install go-plus
-apm install intentions
 apm install language-docker
-apm install language-haml
-apm install language-javascript-jsx
 apm install linter
 apm install linter-docker
-apm install linter-flake8
 apm install linter-ui-default
+apm install multi-cursor
 apm install prettier-atom
 apm install sort-lines
 apm install split-diff
--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,3 @@
 # This cleans up the prompt for times when you are the normal user
 # user@hostname will be shown at other times (e.g. ssh sessions)
 echo "DEFAULT_USER=$(whoami)" >> ~/.zshrc
-
-# Change the users default shell
-chsh -s /usr/local/bin/zsh
