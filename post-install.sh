93d93d9aeb941e4fc1fb9cabec7a59bbdea054bd
Greg Cook <greg@gregcook.com>
2019-01-22 15:34:45 +1100

    change method of setting DEFAULT_USER

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,6 @@
 # Set DEFAULT_USER to suppress username in context
 # This cleans up the prompt for times when you are the normal user
 # user@hostname will be shown at other times (e.g. ssh sessions)
-echo "DEFAULT_USER=$(whoami)" >> ~/.zshrc
+echo "DEFAULT_USER=$(whoami)" > ~/.shell/.user.sh
 
 echo "Setup complete, if you haven't already done so, please run './editor/install.sh' to setup your editor/s"
