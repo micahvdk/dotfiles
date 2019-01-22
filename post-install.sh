d6317d0d38fdb81e685fb1f968780face9843546
Greg Cook <greg@gregcook.com>
2019-01-22 11:55:33 +1100

    add post-install message about editor

--- post-install.sh
+++ post-install.sh
@@ -4,3 +4,5 @@
 # This cleans up the prompt for times when you are the normal user
 # user@hostname will be shown at other times (e.g. ssh sessions)
 echo "DEFAULT_USER=$(whoami)" >> ~/.zshrc
+
+echo "Setup complete, if you haven't already done so, please run './editor/install.sh' to setup your editor/s"
