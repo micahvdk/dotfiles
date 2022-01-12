52ad694d2ead625e0f77643288a608622aec0bd4
Micah van der Kolk <micahvdk@gmail.com>
2022-01-12 23:03:42 +1100

    add brew bash library

---
+++ custom/libraries/brew.sh
@@ -0,0 +1,19 @@
+# Install brew
+brew::install() {
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+  else
+    echo "Homebrew already installed, updating..."
+    brew update
+  fi
+}
+
+# Install apps & packages with Brewfile
+brew::install::packages(){
+  echo "Installing packages and apps with Homebrew & Cask..."
+  brew bundle --file=dependencies/Brewfile --no-lock --no-upgrade
+  if [[ $(uname) == "Darwin" ]]; then
+    brew bundle --file=dependencies/Brewfile.cask --no-lock --no-upgrade
+  fi
+}
