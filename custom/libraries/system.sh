e278a2cfcbecc2ffe7ec62ca22aa6206ab9a2e55
Micah van der Kolk <micahvdk@gmail.com>
2022-01-12 23:29:01 +1100

    fix shell in linux

--- custom/libraries/system.sh
+++ custom/libraries/system.sh
@@ -2,7 +2,7 @@ system::set::default_shell() {
   if [[ $(uname) == "Linux" ]]; then
   # Change the users default shell
   echo "Changing the default shell..."
-  sudo chsh -s /usr/bin/zsh $USER
+  sudo chsh -s /home/linuxbrew/.linuxbrew/bin/zsh $USER
   else
   echo "Changing the default shell..."
   sudo chsh -s /usr/local/bin/zsh $USER
