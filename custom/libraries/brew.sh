6ba59d93023ab0be89bda18ac8a97bb5a580e25b
Micah van der Kolk <micahvdk@gmail.com>
2022-01-13 12:27:51 +1100

    source depending on shell

--- custom/libraries/brew.sh
+++ custom/libraries/brew.sh
@@ -6,6 +6,11 @@ brew::set::path(){
     echo 'PATH="$PATH":/home/linuxbrew/.linuxbrew/bin' >> ~/.bashrc
     echo 'PATH="$PATH":/home/linuxbrew/.linuxbrew/bin' >> ~/.zshrc
   fi
+  if [[ "${SHELL}" == *"bash"* ]]; then
+    source ~/.bashrc
+  elif [[ "${SHELL}" == *"zsh"* ]]; then
+    source ~/.zshrc
+  fi
 }
 
 # Install brew
