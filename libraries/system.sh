b0b105376b730ea1618ae9efe7301945aed59aca
micah <micah@oneflare.com>
2022-02-03 11:09:27 +1100

    add switch to ssh function to dotfiles

--- libraries/system.sh
+++ libraries/system.sh
@@ -20,3 +20,38 @@ system::export::path(){
     echo "export PATH='"${PATH}"'"
   fi
 }
+
+system::git::switch_to_ssh(){
+  #/bin/bash
+  #-- Script to automate https://help.github.com/articles/why-is-git-always-asking-for-my-password
+
+  REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+  if [ -z "$REPO_URL" ]; then
+    echo "-- ERROR:  Could not identify Repo url."
+    echo "   It is possible this repo is already using SSH instead of HTTPS."
+    return 1
+  fi
+
+  USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*)#\1#p'`
+  if [ -z "$USER" ]; then
+    echo "-- ERROR:  Could not identify User."
+    return 1
+  fi
+
+  REPO=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*)#\2#p'`
+  if [ -z "$REPO" ]; then
+    echo "-- ERROR:  Could not identify Repo."
+    return 1
+  fi
+
+  NEW_URL="git@github.com:$USER/$REPO.git"
+  echo "Changing repo url from "
+  echo "  '$REPO_URL'"
+  echo "      to "
+  echo "  '$NEW_URL'"
+  echo ""
+
+  git remote set-url origin "$NEW_URL"
+
+  echo "Success"
+}
